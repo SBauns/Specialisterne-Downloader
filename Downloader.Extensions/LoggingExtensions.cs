09305e6d0fe82c3ba3eacc84d86891dbfcac535d
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-02-23 13:53:32 +0100

    feature: creates barebone implementation of IDownloadService

---
+++ Downloader.Extensions/LoggingExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Downloader.Abstraction.Interfaces.Model;
+using Microsoft.Extensions.Logging;
+
+namespace Downloader.Extensions
+{
+    public static class LoggingExtensions
+    {
+        public static IDisposable BeginTargetScope(this ILogger logger, IDownloadTarget target)
+        {
+            if (target is null) throw new ArgumentNullException(nameof(target));
+            if (string.IsNullOrWhiteSpace(target.OutputFileName))
+                throw new ArgumentException("OutputFileName must be set.", nameof(target));
+
+            return logger.BeginScope("Download: {Download}", target.OutputFileName) ?? NullScope.Instance;
+        }
+
+        private sealed class NullScope : IDisposable
+        {
+            public static readonly NullScope Instance = new();
+
+            private NullScope()
+            {
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+
+}
---
+++ Downloader.Service/DownloadService.cs
@@ -0,0 +1,31 @@
+﻿using Downloader.Abstraction.Interfaces.Model;
+using Downloader.Abstraction.Interfaces.Services;
+using Downloader.Extensions;
+using Downloader.Model;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Downloader.Service
+{
+    public class DownloadService : IDownloadService
+    {
+        private readonly ILogger<DownloadService> logger;
+        private readonly IFileService fileService;
+        private readonly IOptions<DownloaderSettings> options;
+
+        public DownloadService(ILogger<DownloadService> logger, IFileService fileService, IOptions<DownloaderSettings> options)
+        {
+            this.logger = logger;
+            this.fileService = fileService;
+            this.options = options;
+        }
+
+        /// <inheritdoc />
+        public async Task<IDownloadTarget> DownloadContent(IDownloadTarget target)
+        {
+            using var scope = logger.BeginTargetScope(target);
+
+            return target;
+        }
+    }
+}
