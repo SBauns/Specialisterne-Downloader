c5a539c79ca0e416becdd85a96f7e4b8136035b2
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-02-23 13:54:10 +0100

    chore: creates empty implementation of IReportService.

---
+++ Downloader.Service/MarkdownReportService.cs
@@ -0,0 +1,14 @@
+﻿using Downloader.Abstraction.Interfaces.Model;
+using Downloader.Abstraction.Interfaces.Services;
+
+namespace Downloader.Service
+{
+    public class MarkdownReportService : IReportService
+    {
+        /// <inheritdoc />
+        public string GenerateReport(IList<IDownloadTarget> targets)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
